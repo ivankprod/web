db2ef42953b31520db95ca39ae1b1cda494dcbd2
ivankprod <houseprotector@mail.ru>
2022-07-05 03:14:20 +0500

    chore: some explanation

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,12 +9,12 @@ const config: GatsbyConfig = {
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
-	plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+	plugins: [/*"gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap",*/ {
 		resolve: 'gatsby-plugin-manifest',
 		options: {
 			"icon": "src/images/icon.png"
 		}
-	}, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+	}/*, "gatsby-plugin-sharp"*/, "gatsby-transformer-sharp", {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				"name": "images",
