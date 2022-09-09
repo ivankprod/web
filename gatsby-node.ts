a12320e52de7fa3420e62cb4fa7ffdfb23e1b061
ivankprod <houseprotector@mail.ru>
2022-09-10 03:08:05 +0500

    fix(config): gatsby-node.ts deleted

--- gatsby-node.ts
+++
@@ -1,9 +0,0 @@
-import { CreatePageArgs } from "gatsby";
-
-exports.onCreatePage = async ({ page, actions }: CreatePageArgs) => {
-	const { deletePage } = actions;
-
-	if (page.path.endsWith(".html")) {
-		deletePage(page)
-	}
-};
