0334f4cbd778af38adb0e0fc9cc1dd845f1a11da
ivankprod <houseprotector@mail.ru>
2022-09-10 01:56:21 +0500

    build(config): .html pages deletion added

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,15 +1,9 @@
 import { CreatePageArgs } from "gatsby";
 
 exports.onCreatePage = async ({ page, actions }: CreatePageArgs) => {
-	const { createRedirect } = actions;
+	const { deletePage } = actions;
 
 	if (page.path.endsWith(".html")) {
-		const newUrl = page.path.replace(/\.html$/, "");
-
-		createRedirect({
-			fromPath: page.path,
-			toPath: newUrl,
-			statusCode: 404
-		});
+		deletePage(page)
 	}
 };
