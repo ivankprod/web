715d5f30c2656e5fd4225a3eb0836e70d10566c1
ivankprod <houseprotector@mail.ru>
2022-09-09 23:50:24 +0500

    refactor(layout): Footer component extracted

---
+++ src/components/footer.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+const Footer: React.FC = () => {
+	const { site } = useStaticQuery(
+		graphql`query {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}`
+	)
+
+	return (
+		<>
+			<footer>
+				<div className="footer-back"></div>
+				<div className="container">
+					<div className="footer-block first">
+						<div className="footer-block-title">ИНФО</div>
+						© 2008-{new Date().getFullYear()} {site.siteMetadata.title}. Все права защищены.
+						<br/>
+						Копирование материлов сайта без указания источника запрещено!
+						<br/>
+						<br/>
+						<Link to="/legal/user-agreement/">Пользовательское соглашение</Link>
+						<br/>
+						<Link to="/legal/privacy-policy/">Политика конфиденциальности</Link>
+					</div>
+					<div className="footer-block second">
+						<div className="footer-block-title">БЫСТРЫЕ ССЫЛКИ</div>
+						<ul>
+							<li><Link to="/projects/">ПРОЕКТЫ</Link></li>
+							<li><Link to="/services/">УСЛУГИ</Link></li>
+							<li><Link to="/blog/">БЛОГ</Link></li>
+							<li><Link to="/about/">О НАС</Link></li>
+							<li><Link to="/contacts/">КОНТАКТЫ</Link></li>
+							<li><Link to="/join/">ЗАПОЛНИТЬ БРИФ</Link></li>
+							<li><Link to="/sitemap/">КАРТА САЙТА</Link></li>
+						</ul>
+					</div>
+					<div className="footer-block third">
+						<div className="footer-block-title">СОЦИАЛЬНЫЕ СЕТИ</div>
+						<ul className="social">
+							<li><a className="vk" href="https://vk.com/ivankprod" title="Группа во ВКонтакте" target='blank'></a></li>
+							<li><a className="fb" href="https://facebook.com/ivankprod" title="Группа в Facebook" target='blank'></a></li>
+							<li><a className="tw" href="https://twitter.com/ivankprod" title="Профиль в Twitter" target='blank'></a></li>
+							<li><a className="ig" href="https://instagram.com/ivankprod" title="Профиль в Instagram" target='blank'></a></li>
+							<li><a className="sc" href="https://soundcloud.com/ivankprod" title="Страница на SoundCloud" target='blank'></a></li>
+							<li><a className="yt" href="https://youtube.com/c/IvanKProduction" title="Официальный YouTube-канал" target='blank'></a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</>
+	)
+}
+
+export default Footer
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+import Footer from "./footer"
 
 import "normalize.css"
 
@@ -21,45 +23,7 @@ const Layout: React.FC<LayoutProps> = ({ children = {} }) => {
 	return (
 		<>
 			<main>{children}</main>
-			<footer>
-				<div className="footer-back"></div>
-				<div className="container">
-					<div className="footer-block first">
-						<div className="footer-block-title">ИНФО</div>
-						© 2008-{new Date().getFullYear()} {site.siteMetadata.title}. Все права защищены.
-						<br/>
-						Копирование материлов сайта без указания источника запрещено!
-						<br/>
-						<br/>
-						<a className="spa" href="/legal/user-agreement/">Пользовательское соглашение</a>
-						<br/>
-						<a className="spa" href="/legal/privacy-policy/">Политика конфиденциальности</a>
-					</div>
-					<div className="footer-block second">
-						<div className="footer-block-title">БЫСТРЫЕ ССЫЛКИ</div>
-						<ul>
-							<li><a href="/projects/">ПРОЕКТЫ</a></li>
-							<li><a href="/services/">УСЛУГИ</a></li>
-							<li><a href="/blog/">БЛОГ</a></li>
-							<li><a href="/about/">О НАС</a></li>
-							<li><a href="/contacts/">КОНТАКТЫ</a></li>
-							<li><a href="/join/">ЗАПОЛНИТЬ БРИФ</a></li>
-							<li><a href="/sitemap/">КАРТА САЙТА</a></li>
-						</ul>
-					</div>
-					<div className="footer-block third">
-						<div className="footer-block-title">СОЦИАЛЬНЫЕ СЕТИ</div>
-						<ul className="social">
-							<li><a className="vk" href="https://vk.com/ivankprod" title="Группа во ВКонтакте" target='blank'></a></li>
-							<li><a className="fb" href="https://facebook.com/ivankprod" title="Группа в Facebook" target='blank'></a></li>
-							<li><a className="tw" href="https://twitter.com/ivankprod" title="Профиль в Twitter" target='blank'></a></li>
-							<li><a className="ig" href="https://instagram.com/ivankprod" title="Профиль в Instagram" target='blank'></a></li>
-							<li><a className="sc" href="https://soundcloud.com/ivankprod" title="Страница на SoundCloud" target='blank'></a></li>
-							<li><a className="yt" href="https://youtube.com/c/IvanKProduction" title="Официальный YouTube-канал" target='blank'></a></li>
-						</ul>
-					</div>
-				</div>
-			</footer>
+			<Footer/>
 		</>
 	)
 }
