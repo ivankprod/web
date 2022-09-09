86110a31023541a0e2e742a0a3a093d582a2d476
ivankprod <houseprotector@mail.ru>
2022-09-09 21:38:50 +0500

    feat(styles): normalize.css added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,7 @@
-import React, { ReactNode } from "react"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+
+import "normalize.css"
 
 interface LayoutProps {
 	children?: any
