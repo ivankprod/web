a92eb94edc6fe8a7f4cfe62783a6220c1cbda9e2
ivankprod <houseprotector@mail.ru>
2022-11-08 03:24:06 +0500

    refactor(css): refactor css to use nested rules, close #27 issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
 	plugins: [
 		require('autoprefixer')({ remove: false }),
-		require('postcss-preset-env')
+		require('postcss-preset-env')({
+			stage: 3,
+			features: {
+				'nesting-rules': true
+			}
+		})
 	]
 };
